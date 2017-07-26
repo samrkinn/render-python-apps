9961344c5141798cff2c1cbac8ffb8d0f54fc991
forrest collman <forrest.collman@gmail.com>
2017-07-26 09:44:11 -0700

    changing way we do affine application

--- renderapps/stack/apply_global_affine_to_stack.py
+++ renderapps/stack/apply_global_affine_to_stack.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 import renderapi
-from renderapi.transform import AffineModel
+from renderapi.transform import AffineModel, ReferenceTransform
 from ..module.render_module import RenderModule, RenderParameters
 from functools import partial
 import tempfile
 import os
 import numpy as np
 from argschema.fields import Str, Float, Int
+import json
 
 #An example set of parameters for this module
 example_parameters = {
@@ -18,14 +19,14 @@ example_parameters = {
         "client_scripts":"/pipeline/render/render-ws-java-client/src/main/scripts"
     },
     'input_stack':'EM_Site4_stitched',
-    'output_stack':'EM_Site4_stitched',
-    'M00':1,
+    'output_stack':'EM_Site4_stitched_BIG',
+    'M00':33.0,
     'M10':0,
     'M01':0,
-    'M11':1,
-    'B0':2*15294,
-    'B1':-2*1515,
-    'pool_size':20
+    'M11':33.0,
+    'B0':0,
+    'B1':0,
+    'pool_size':2
 }
 
 class ApplyAffineParameters(RenderParameters):
@@ -43,18 +44,23 @@ class ApplyAffineParameters(RenderParameters):
 
 #define a function to process one z value
 def process_z(render,input_stack,tform,z):
+    
+    changes_list =[]
     #get the tilespecs for this Z
     tilespecs = render.run( renderapi.tilespec.get_tile_specs_from_z,
                             input_stack,
                             z)
     #loop over the tilespes adding the transform
     for ts in tilespecs:
-        ts.tforms.append(tform)
+        d={'tileId':ts.tileId,
+          'transform':tform.to_dict()}
+        changes_list.append(d)
+       
     #open a temporary file
     tid,tfile = tempfile.mkstemp(suffix='.json')
     file = open(tfile,'w')
     #write the file to disk
-    renderapi.utils.renderdump(tilespecs,file)
+    json.dump(changes_list,file)
     os.close(tid)
     #return the filepath
     return tfile
@@ -66,49 +72,61 @@ class ApplyAffine(RenderModule):
         super(ApplyAffine,self).__init__(schema_type=schema_type,*args,**kwargs)
 
     def run(self):
-        self.logger.error('WARNING NEEDS TO BE TESTED, TALK TO FORREST IF BROKEN')
-
         #get the z values in the stack
         zvalues = self.render.run(renderapi.stack.get_z_values_for_stack,self.args['input_stack'])
         zvalues = np.array(zvalues)
-        print zvalues
-        zmin = self.argsargs.get('zmin',np.min(zvalues))
-        zmax = self.argsargs.get('zmax',np.max(zvalues))
+        print(zvalues)
+        zmin = self.args.get('zmin',np.min(zvalues))
+        zmax = self.args.get('zmax',np.max(zvalues))
         zvalues = zvalues[zvalues>=zmin]
         zvalues = zvalues[zvalues<=zmax]
 
-        #define the affine transform to apply everywhere
-        global_tform = AffineModel(M00=self.argsargs['M00'],
-                            M10=self.argsargs['M10'],
-                            M01=self.argsargs['M01'],
-                            M11=self.argsargs['M11'],
-                            B0=self.argsargs['B0'],
-                            B1=self.argsargs['B1'])
+
 
         #output_stack defaults to input_stack
-        output_stack = self.args.get('output_stack',self.args['input_stack'])
+        input_stack = self.args['input_stack']
+        output_stack = self.args.get('output_stack',input_stack)
 
+        tformid = '{}_to_{}'.format(input_stack,output_stack)
+
+        #define the affine transform to apply everywhere
+        global_tform = AffineModel(M00=self.args['M00'],
+                            M10=self.args['M10'],
+                            M01=self.args['M01'],
+                            M11=self.args['M11'],
+                            B0=self.args['B0'],
+                            B1=self.args['B1'],
+                            transformId =  tformid)
+        global_tform_ref = ReferenceTransform(refId=tformid)
+       
         #define a processing pool
         #pool = Pool(self.argsargs['pool_size'])
         #define a partial function for processing a single z value
+        
+        #make the first json fiel with the actual transform
+        json_0 = process_z(self.render, input_stack,global_tform,zvalues[0])
+        #do the rest using the reference
         mypartial = partial(process_z,
-                            self.args.render,
-                            self.args['input_stack'],
-                            global_tform)
+                            self.render,
+                            input_stack,
+                            global_tform_ref)
         #get the filepaths of json files in parallel
-        #json_files = []
-        #for z in zvalues:
-        #    print z
-        #    json_files.append(mypartial(z))
+        json_files = []
+        for z in zvalues:
+            print z
+            json_files.append(mypartial(z))
         #print json_files
-        with renderapi.client.WithPool(self.args['pool_size']) as pool:
-            json_files = pool.map(mypartial,zvalues)
+        #with renderapi.client.WithPool(self.args['pool_size']) as pool:
+        #    json_files = pool.map(mypartial,zvalues[1:])
+        json_files =[json_0]+json_files
         #import the json_files into the output stack
         if (self.args['input_stack'] != output_stack):
             self.render.run(renderapi.stack.create_stack,output_stack)
 
-        renderapi.client.import_jsonfiles_parallel( output_stack,
+        renderapi.client.importTransformChangesClient(input_stack,
+                                                    output_stack,
                                                     json_files,
+                                                    changeMode = 'APPEND',
                                                     poolsize=self.args['pool_size'],
                                                     render=self.render)
 
@@ -117,5 +135,5 @@ class ApplyAffine(RenderModule):
 
 
 if __name__ == "__main__":
-    mod = ApplyAffine(input_data= example_json)
+    mod = ApplyAffine(input_data= example_parameters)
     mod.run()
